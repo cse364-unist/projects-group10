f32c168dcb580a14f23e8ce6edaaed92bdef5498
20201030KiHun <164668212+20201030KiHun@users.noreply.github.com>
2024-04-23 15:08:08 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,7 @@ cd milestone1
 
 mvn package
 
+git clone https://github.com/cse364-unist/projects-group10.git
+git checkout milestone2
+
 java -jar ./target/cse364-project-1.0-SNAPSHOT-with-dependencies.jar

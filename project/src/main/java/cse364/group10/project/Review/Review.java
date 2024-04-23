4c7d6ac198fa22f13799a3bcde94ca3f35f7b88f
UnkyungJo <123553106+UnkyungJo@users.noreply.github.com>
2024-04-24 01:11:34 +0900

    Add files via upload

--- project/src/main/java/cse364/group10/project/Review/Review.java
+++ project/src/main/java/cse364/group10/project/Review/Review.java
@@ -1,9 +1,20 @@
 package cse364.group10.project.Review;
 
 import cse364.group10.project.Users.Users;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Review {
+    @id @GeneratedValue
+    private Long id;
     private Users user;
     private String comments;
+
+    public Long getId() {
+        return id;
+    }
 
     public Users getUser() {
         return user;
@@ -11,6 +22,10 @@ public class Review {
 
     public String getComments() {
         return comments;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setUser(Users user) {

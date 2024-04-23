884766e73a71414bcec33b89c04a7c701953d106
UnkyungJo <123553106+UnkyungJo@users.noreply.github.com>
2024-04-24 01:12:10 +0900

    Add files via upload

--- project/src/main/java/cse364/group10/project/Users/Users.java
+++ project/src/main/java/cse364/group10/project/Users/Users.java
@@ -1,14 +1,27 @@
 package cse364.group10.project.Users;
 
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Users {
+    @id @GeneratedValue
+    private Long id;
     private String name;
     private String password;
     private String email;
 
     Users() {}
 
-    Users(String name, String password, String email) {
+    public Users SignUp(String name, String password, String email) {
         this.name = name; this.password = password; this.email = email;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
@@ -21,6 +34,10 @@ public class Users {
 
     public String getEmail() {
         return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setName(String name) {

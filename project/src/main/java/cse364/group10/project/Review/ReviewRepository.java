d7878b7ac2d5764f72e37ee7901f1bc8d786e7f4
UnkyungJo <123553106+UnkyungJo@users.noreply.github.com>
2024-04-24 01:17:19 +0900

    Add files via upload

---
+++ project/src/main/java/cse364/group10/project/Review/ReviewRepository.java
@@ -0,0 +1,8 @@
+package cse364.group10.project.Review;
+
+import cse364.group10.project.Review.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}

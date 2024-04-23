6fd1289560097f742ed6373cee4759ef7fb5062f
UnkyungJo <123553106+UnkyungJo@users.noreply.github.com>
2024-04-23 19:17:31 +0900

    maven 프로젝트 추가

---
+++ project/src/test/java/cse364/group10/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cse364.group10.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

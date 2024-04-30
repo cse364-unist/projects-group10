3b081765aa030f16ee67138d5fe707272159d05e
UnkyungJo <123553106+UnkyungJo@users.noreply.github.com>
2024-04-30 22:32:51 +0900

    폴더 이름 바뀌어서 삭제

--- project/src/main/java/cse364/group10/project/Users/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cse364.group10.project.Users;
-
-public class UserNotFoundException extends  RuntimeException {
-    UserNotFoundException(Long id) {
-        super("Could not find user " + id);
-    }
-}

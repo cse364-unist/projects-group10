42a3a11624a376235acf32e4dc8e3a01c72130f1
Jonojono657 <123913890+Jonojono657@users.noreply.github.com>
2024-05-03 15:09:56 +0900

    Update MovieController.java
    Id를 string으로 수정

--- project/src/main/java/cse364/group10/project/Movie/MovieController.java
+++ project/src/main/java/cse364/group10/project/Movie/MovieController.java
@@ -2,15 +2,11 @@ package cse364.group10.project.Movie;
 
 import java.util.List;
 
-import cse364.group10.project.Review.KeywordExtract.KeyWordExtractor;
-
-import cse364.group10.project.Review.ReviewController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MovieController {
-    private final MovieRepository repository;
+    private  final MovieRepository repository;
 
     MovieController(MovieRepository repository) {
         this.repository = repository;
@@ -21,31 +17,31 @@ public class MovieController {
         return repository.findAll();
     }
 
-    @PostMapping("/movies")
+    @PostMapping("movies")
     Movie newMovie(@RequestBody Movie newMovie) {
         return repository.save(newMovie);
     }
 
     @GetMapping("/movies/{id}")
-    Movie one(@PathVariable Long id) {
+    Movie one(@PathVariable String id) {
         return repository.findById(id)
                 .orElseThrow(() -> new MovieNotFoundException(id));
     }
 
-    @GetMapping("/movies")
-    List<Movie> findByGenre(@RequestParam("genre") String genre) {
+    @GetMapping("/movies?genre={genre}")
+    List<Movie> findByGenre(@PathVariable String genre) {
         return repository.findByGenre(genre);
     }
 
     @PutMapping("/movies/{id}")
-    Movie replaceMovie(@RequestBody Movie newMovie, @PathVariable Long id) {
+    Movie replaceMovie(@RequestBody Movie newMovie, @PathVariable String id) {
         return repository.findById(id)
                 .map(movie -> {
                     movie.setAvgRating(newMovie.getAvgRating());
-                    movie.setDiscription(newMovie.getDiscription());
+                    movie.setDescription(newMovie.getDescription());
                     movie.setGenre(newMovie.getGenre());
                     movie.setTitle(newMovie.getTitle());
-                    movie.setTumbnail(newMovie.getTumbnail());
+                    movie.setThumbnail(newMovie.getTumbnail());
                     return repository.save(movie);
                 })
                 .orElseGet(() -> {
@@ -54,13 +50,8 @@ public class MovieController {
                 });
     }
 
-    @GetMapping("/movies/{genre}")
-    List<Movie> getMoviesWithGenre(@PathVariable("genre") String genre) {
-        return repository.findByGenre(genre);
-    }
-
     @DeleteMapping("/movies/{id}")
-    void deleteMovie(@PathVariable Long id) {
+    void deleteMovie(@PathVariable String id) {
         repository.deleteById(id);
     }
 }

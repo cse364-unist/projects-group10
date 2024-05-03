4f7e13fdf7996891f95952439db49179b010672b
Jonojono657 <123913890+Jonojono657@users.noreply.github.com>
2024-05-03 15:11:15 +0900

    Update MovieRepository.java
    Id를 string으로 수정

--- project/src/main/java/cse364/group10/project/Movie/MovieRepository.java
+++ project/src/main/java/cse364/group10/project/Movie/MovieRepository.java
@@ -1,8 +1,8 @@
 package cse364.group10.project.Movie;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import  java.util.List;
+import java.util.List;
 
-public interface MovieRepository extends  MongoRepository<Movie, Long> {
+public interface MovieRepository extends  MongoRepository<Movie, String> {
     List<Movie> findByGenre(String genre);
 }

3c276af56ad515a5bedb0e075e2df5b7db3d2fe1
Jonojono657 <123913890+Jonojono657@users.noreply.github.com>
2024-05-03 23:03:59 +0900

    원래대로

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
-cd project
+git clone https://github.com/cse364-unist/projects-group10.git
+cd projects-group10
+git checkout milestone2
+
+mvn jacoco:report
 
 mvn package
 
-java -jar ./target/project-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+java -jar target/project-0.0.1-SNAPSHOT.jar
